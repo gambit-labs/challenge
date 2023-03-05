1235cc974546d5d521d6f6199f1396d34ed9eedd
Ghh84 <ghiwethh@gmail.com>
2023-03-05 13:51:49 +0200

    Pagination is added to the `displayResult` components

--- gambit/src/components/displayResult.jsx
+++ gambit/src/components/displayResult.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import Table from './common/table'
+import Pagination from './common/pagination'
+import { paginate } from '../utils/paginate'
 
 class DisplayResult extends Component {
   state = {
+    currentPage: 1,
+    pageSize: 8,
     sortColumn: { path: 'index', order: 'asc' },
 
     columns: [
@@ -17,7 +21,9 @@ class DisplayResult extends Component {
 
   render() {
     const { readingData } = this.props
-    const { columns, sortColumn } = this.state
+    const { pageSize, currentPage, columns, sortColumn } = this.state
+    const startIndex = (currentPage - 1) * pageSize
+    const paginatedData = paginate(readingData, currentPage, pageSize)
 
     if (readingData.length === 0)
       return <p>There are no reading in the database.</p>
@@ -26,9 +32,18 @@ class DisplayResult extends Component {
       <div>
         <Table
           columns={columns}
-          paginatedData={readingData}
+          paginatedData={paginatedData}
+          startIndex={startIndex}
           sortColumn={sortColumn}
         />
+        <div className="table">
+          <Pagination
+            itemsCount={readingData.length}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
+        </div>
       </div>
     )
   }
