d9ced5ad2b956428985e34be27c671658ecc031d
Ghh84 <ghiwethh@gmail.com>
2023-03-05 12:21:39 +0200

    intial gambit chalenge app

---
+++ gambit/src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import axios from 'axios'
+import "./App.css";
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() { 
+    //http://tuftuf.gambitlabs.fi/feed.txt
+    const {data:posts}=await axios.get('https://jsonplaceholder.typicode.com/posts')
+    this.setState({posts})
+   }
+
+  handleAdd = () => {
+    console.log("Add");
+  };
+
+  handleUpdate = post => {
+    console.log("Update", post);
+  };
+
+  handleDelete = post => {
+    console.log("Delete", post);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
