8660671598d11f1565f78edd40d182193967d81d
Ghh84 <ghiwethh@gmail.com>
2023-03-06 05:19:00 +0200

    Restructure the App and adding of routes

--- gambit/src/App.js
+++ gambit/src/App.js
@@ -1,77 +1,23 @@
-import { useEffect, useState } from "react";
-import Footer from "./components/footer";
-import DisplayResult from "./components/displayResult";
-import SearchBox from "./components/common/searchBox";
-import LoginForm from "./components/loginForm";
-import { getCurrentUser } from "./services/auth.service";
-import "./App.css";
-const App = () => {
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import GetData from './components/getData';
+import Login from './components/login';
+import Logout from './components/logout';
+import './App.css';
 
-  const [data, setData] = useState([]);
-  const [dataContent, setDataContent] = useState([]);
-  const [searchquery, setSearchquery]=useState()
-  const [user, setUser]=useState('')
-  const dataObj = {};
-   
-  const fetchData = () => {
-    let corsHttpAddress = "https://cors-anywhere.herokuapp.com/";
-    let gambitLabsAddress = "http://tuftuf.gambitlabs.fi/feed.txt";
-    fetch(corsHttpAddress + gambitLabsAddress)
-      .then((response) => response.text())
-      .then((result) => {
-        const theDataArr = result.replace(/\n/g, " ");
-        const array = theDataArr.split(" ");
-        setData(array);
-      });
-  };
-
-  useEffect(() => {    
-    const user = getCurrentUser();
-    setUser(user);    
-    fetchData();
-    data.forEach((val) => {
-      var key = val.split(":")[0];
-      var value = val.split(":")[1];
-      dataObj[key] = value;
-    });
-    const k = Object.values(dataObj);
-    setDataContent(k);
-  
-  }, [data, searchquery]);
-
-   return (
-    <div>
-      <header className="header">
-        <div >
-          <h1 className="logo">TUF-2000M Registers</h1>              
-        </div>
-        <div className="searchBox">
-          <SearchBox value={searchquery} onChange={(e) => setSearchquery(e.target.value)} />
-        </div>
-        <div>
-        <p>
-            Registery Date:{" "}
-            <span className="registryTime">
-              {`${data[0]} ${data[1]} `}
-            </span>
-          </p></div> 
-                
-      </header>
-      <main className="main">
-        {(user===null)? 
-        <LoginForm />:
-        <DisplayResult         
-        readingData={dataContent}
-        searchquery={searchquery}      
-        />  
-        }         
-      </main>
-      <footer className="footer" > 
-          <Footer />
-      </footer>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route element={<Login/>} path='/login' exact/>
+          <Route element={<Login/>} path='/' exact/>
+          <Route element={<GetData/>} path='/display' exact/>
+          <Route element={<Logout/>} path='/Logout' exact/> 
+        </Routes>
+      </Router>
+    );
+  }
 }
-
 
 export default App;
