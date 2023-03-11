67ed2fc04036932adedcfe93300ad22345c78286
Ghh84 <ghiwethh@gmail.com>
2023-03-11 12:59:39 +0200

    Remove api folder

--- gambit/api/middleware/authJwt.js
+++
@@ -1,9 +0,0 @@
-const verifyToken = (req, res, next) => {
-    console.log('Came on verify token....')
-    next();
-};
-
-const authJwt = {
-    verifyToken: verifyToken
-};
-module.exports = authJwt;
