0ff78e3bf445cc8d57f182e3a2ea6f756c4af0ea
Ghh84 <ghiwethh@gmail.com>
2023-03-11 12:51:03 +0200

    Add error handling

--- gambit/src/App.js
+++ gambit/src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Navigate} from 'react-router-dom';
 import DataComponent from './components/dataComponent';
 import Login from './components/login';
 import Logout from './components/logout'
-import RegisterForm from './components/registerForm'
 import PublicRoute from './components/route/publicRoute';
-import PrivateRoute from './components/route/privateRoute';
 import './App.css';;
 
 class App extends Component {
@@ -15,7 +13,6 @@ class App extends Component {
           <PublicRoute element={<Login/>} path='/login' exact/>
           <PublicRoute element={<DataComponent/>} path='/display' exact/>
           <PublicRoute element={<Logout/>} path='/Logout' exact/>  
-          <PrivateRoute element={<RegisterForm/>} path='/register' exact />
           <PublicRoute  element={<Navigate replace to="/login" /> } path='' />
       </Router>
     );
